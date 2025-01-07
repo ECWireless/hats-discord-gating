a87a4a0669aac387773e300106b9a3b021644f89
ECWireless <elliott@coopallc.com>
2025-01-07 08:18:59 -0700

    Add chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,27 @@
 import type { AppProps } from "next/app";
+import {
+  ChakraProvider,
+  createSystem,
+  defaultConfig,
+  defineConfig,
+} from "@chakra-ui/react";
+
+const config = defineConfig({
+  globalCss: {
+    "html, body": {
+      background: "rgb(219 234 254/100%)",
+      margin: 0,
+      padding: 0,
+    },
+  },
+});
+
+const system = createSystem(defaultConfig, config);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider value={system}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
